e0cdfed3c0148008d17024744a08014fc72481a1
Justine Batiforra <jbatiforra03@gmail.com>
2015-07-22 12:33:00 -0700

    Server file contains AJAX call - end of class

--- hapi.js
+++ hapi.js
@@ -1,7 +1,7 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({port: 3000});
 
 
 // routes
@@ -13,6 +13,49 @@ server.route({
             path: 'public',
             listing: true
         }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/flickr',
+    handler: function (request, reply) {
+        var credentials = require('./public/js/credentials.js'), // "./" means current folder; same as <script src="public/js/credentials.js"></script>
+            httpRequest = require('request'),
+            data = {
+                "method": 'flickr.photos.search',
+                "api_key": credentials.flickr.api_key,
+                "tags": 'vancouver',
+                "format": 'json',
+                "nojsoncallback": 1
+            }, 
+            options = {
+                "uri": 'https://api.flickr.com/services/rest/', 
+                "qs": data
+            };
+        
+        httpRequest(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                reply(body); // Show the HTML for the Google homepage in CLI (cmd line interface)
+            }
+        });
+        
+        console.log('Flickr content coming soon'); // browser output
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/google',
+    handler: function (request, reply) {
+        var httpRequest = require('request');
+        
+        httpRequest('http://www.google.com', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                reply(body); // Show the HTML for the Google homepage in CLI (cmd line interface)
+                console.log('CLI'); // browser output
+            }
+        });
     }
 });
 
--- public/js/credentials.js
+++ public/js/credentials.js
@@ -3,3 +3,7 @@ var credentials = {
         "api_key": '75f53830cbb439e739f7de763a129fd4'
     }
 };
+
+if (typeof module !== "undefined") { // server; what are we exporting/making public?
+    module.exports = credentials;
+}
