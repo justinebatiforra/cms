1b59e1e8baf8282c5f992e588af11a2ee193dff1
Justine Batiforra <jbatiforra03@gmail.com>
2015-07-22 11:33:05 -0700

    Loop through photo titles in Flickr API response
    Also changed directory/file path for html and js files

---
+++ hapi.js
@@ -0,0 +1,29 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+
+// routes
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            listing: true
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
